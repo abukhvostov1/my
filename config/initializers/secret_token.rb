3eaa312e3b9f22e8c89e395dad8101f039a28758
Alexander <junior@telegraf.by>
2013-01-29 12:52:08 +0300

    test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sasha::Application.config.secret_token = 'f345112a7de216241265cf2eb571d7821063044a17bcf6ce86c4437c1c70fbff725f1d40026ad0dd7ff9cf8db0b54f7396ba37d824ba316d6f96a602a6226cd7'
